3ebd5710ad4e75fa5b8d6903906340ffb67d8705
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-05-05 11:03:05 +0700

    Private Members Belah Ketupat

--- PRAKTIKUM/praktikum.cpp
+++ PRAKTIKUM/praktikum.cpp
@@ -1,9 +1,13 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
 
 class LayangLayang;
 
 class BelahKetupat {
+private:
+    double diagonal1, diagonal2, sisi;
 
-
-class LayangLayang {
-
+public:
+  
