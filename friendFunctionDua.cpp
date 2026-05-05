df9fdc1b8c2db4ede86001e3b6dd01fd588be7ef
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-05-05 10:33:57 +0700

    Pembuatan class manusia sebagai pengelola

--- friendFunctionDua.cpp
+++ friendFunctionDua.cpp
@@ -9,26 +9,3 @@ public:
     void showNilaiPelajar(pelajar &x);
 };
 
-class pelajar
-{
-private:
-    int nilai;
-
-public:
-    pelajar() { nilai = 100; }
-    // Member function dari class manusia dijadikan friend
-    friend void manusia::showNilaiPelajar(pelajar &x);
-};
-
-void manusia::showNilaiPelajar(pelajar &x)
-{
-    cout << x.nilai;
-}
-
-int main()
-{
-    manusia budi;
-    pelajar pbudi;
-    budi.showNilaiPelajar(pbudi);
-    return 0;
-}
