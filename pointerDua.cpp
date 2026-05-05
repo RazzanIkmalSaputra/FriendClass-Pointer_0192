940be2423d15a00599c4901938ce02ec734b7ec8
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-05-05 10:40:53 +0700

    Alokasi memori dinamis dengan new

--- pointerDua.cpp
+++ pointerDua.cpp
@@ -15,8 +15,3 @@ int main()
 {
     // Pointer object dengan alokasi dinamis
     mahasiswa *mhs = new mahasiswa{1}; 
-    mhs->nim = 2;
-    mhs->showNim();
-    delete mhs; // Menghapus alokasi memori
-    return 0;
-}
