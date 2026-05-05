cd7311e2e33b4ef8d911934c1cf9f45fc39c62eb
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-05-05 10:41:36 +0700

    Pembersihan memori dengan delete

--- pointerDua.cpp
+++ pointerDua.cpp
@@ -1,4 +1,22 @@
+#include <iostream>
+using namespace std;
 
+class mahasiswa
+{
+public:
+    int nim;
+    void showNim()
+    {
+        cout << "No Induk = " << nim << endl;
+    }
+};
+
+int main()
+{
+    // Pointer object dengan alokasi dinamis
+    mahasiswa *mhs = new mahasiswa{1}; 
     mhs->nim = 2;
     mhs->showNim();
-  
+    delete mhs; // Menghapus alokasi memori
+    return 0;
+}
