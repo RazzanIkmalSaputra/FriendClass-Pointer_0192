15b722f08d1957ddd1976c8b4cf949297201f8ce
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-05-05 10:41:18 +0700

    Manipulasi data melalui pointer

--- pointerDua.cpp
+++ pointerDua.cpp
@@ -1,17 +1,4 @@
-#include <iostream>
-using namespace std;
 
-class mahasiswa
-{
-public:
-    int nim;
-    void showNim()
-    {
-        cout << "No Induk = " << nim << endl;
-    }
-};
-
-int main()
-{
-    // Pointer object dengan alokasi dinamis
-    mahasiswa *mhs = new mahasiswa{1}; 
+    mhs->nim = 2;
+    mhs->showNim();
+  
