62c24fcd33b4b8dfbf031cf6d17c6ba01c7214eb
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-05-05 11:15:11 +0700

    Method Input Belah Ketupat

--- PRAKTIKUM/praktikum.cpp
+++ PRAKTIKUM/praktikum.cpp
@@ -10,4 +10,11 @@ private:
     double diagonal1, diagonal2, sisi;
 
 public:
-  
+    void input() {
+        cout << "[ Input Belah Ketupat ]" << endl;
+        cout << "Masukkan Diagonal 1: "; cin >> diagonal1;
+        cout << "Masukkan Diagonal 2: "; cin >> diagonal2;
+        cout << "Masukkan Sisi      : "; cin >> sisi;
+    }
+
+ 
