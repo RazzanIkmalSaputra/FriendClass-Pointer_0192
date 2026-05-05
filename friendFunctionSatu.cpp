ea61e1750c1cc45cf7ff0f01e72a18c67c2fef8e
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-05-05 10:33:27 +0700

    Implementasi fungsi dan pengujian di main

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -11,3 +11,16 @@ public:
     friend void setNama(mahasiswa &a, string);
 };
 
+// Definisi fungsi di luar class
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+
+int main()
+{
+    mahasiswa joko;
+    setNama(joko, "Kairi Kumar");
+    return 0;
+}
